7beb69634c18648bb430bd04cef5eb99deb81614
ARNAU SISTACH REINOSO <asistare7.alumnes@ub052229.mat.ub.edu>
2015-09-28 19:51:51 +0200

    fent-ho poc a poc a classe

---
+++ 3Lab/1exercici.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+
+void
+ShowMan (int n, float x, double e, double nivi)
+{	double z = pow (e, n-1);
+	printf ("%d\t %e\t %e\t %e\t %e\t %e\n", n, z, nivi, x, x-z, (x-z)/z);	
+}
+
+int
+main (void)
+{
+	int n;
+	float xn, xv, xt; /* xn es l'ultim valor; xv es el vell */
+	double dn, dv, dt; /* xn es l'ultim valor; xv es el vell */
+	double z;
+	z = 1 - sqrt (3);
+
+	xv = 1;
+	dv = 1;
+	dn = z;
+	xn = 1 - sqrtf (3); /* sqrtf pq treballo amb flotants */
+	printf ("n\tz\td x \tf x\tx-z\t(x-z)/z error abs/ err abs anterior\n");
+
+	for (n = 3; n < 50; n++)
+	{
+		xt = 2* (xn + xv);
+		xv = xn;
+		xn = xt;
+
+		dt = 2* (dn + dv);
+		dv = dn;
+		dn = dt;
+
+		ShowMan (n, xn, z, dn);
+	}
+
+return 0;
+}
